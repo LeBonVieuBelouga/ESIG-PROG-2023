54e9969a5a1d653b22b1c9f1d30d20e311cde183
Léo Küttel <leo.kuettel@gmail.com>
2023-08-30 14:59:39 +0200

    MonoGame Projet Tuto
    Projet de base avec des tests sur le framework

--- src/RogueProject/Game1.cs
+++ src/RogueProject/Game1.cs
@@ -9,6 +9,11 @@ namespace RogueProject
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D AmongUsTexture;
+        Vector2 AmongUs_Pos;
+        float AmongUs_Speed;
+
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,15 +24,21 @@ namespace RogueProject
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            
+            AmongUs_Pos = new Vector2(_graphics.PreferredBackBufferWidth/2,
+                _graphics.PreferredBackBufferHeight/2);
+            AmongUs_Speed = 350f;
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+            // Create a new SpriteBatch, which can be used to draw textures.
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            AmongUsTexture = Content.Load<Texture2D>("BasicPoseV4");
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,6 +48,48 @@ namespace RogueProject
 
             // TODO: Add your update logic here
 
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.Up) || kstate.IsKeyDown(Keys.W))
+            {
+                AmongUs_Pos.Y -= AmongUs_Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (kstate.IsKeyDown(Keys.Down) || kstate.IsKeyDown(Keys.S))
+            {
+                AmongUs_Pos.Y += AmongUs_Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (kstate.IsKeyDown(Keys.Left) || kstate.IsKeyDown(Keys.A))
+            {
+                AmongUs_Pos.X -= AmongUs_Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (kstate.IsKeyDown(Keys.Right) || kstate.IsKeyDown(Keys.D))
+            {
+                AmongUs_Pos.X += AmongUs_Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            //Pour ne pas sortir de la zone
+
+            if (AmongUs_Pos.X > _graphics.PreferredBackBufferWidth - AmongUsTexture.Width / 2)
+            {
+                AmongUs_Pos.X = _graphics.PreferredBackBufferWidth - AmongUsTexture.Width / 2;
+            }
+            else if (AmongUs_Pos.X < AmongUsTexture.Width / 2)
+            {
+                AmongUs_Pos.X = AmongUsTexture.Width / 2;
+            }
+
+            if (AmongUs_Pos.Y > _graphics.PreferredBackBufferHeight - AmongUsTexture.Height / 2)
+            {
+                AmongUs_Pos.Y = _graphics.PreferredBackBufferHeight - AmongUsTexture.Height / 2;
+            }
+            else if (AmongUs_Pos.Y < AmongUsTexture.Height / 2)
+            {
+                AmongUs_Pos.Y = AmongUsTexture.Height / 2;
+            }
+
             base.Update(gameTime);
         }
 
@@ -45,6 +98,19 @@ namespace RogueProject
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(
+                AmongUsTexture,
+                AmongUs_Pos,
+                null,
+                Color.White,
+                0f,
+                new Vector2(AmongUsTexture.Width / 2, AmongUsTexture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
