cadbca9e9122400c2481ca740c5b69067a30b5d0
Léo Küttel <leo.kuettel@gmail.com>
2023-09-04 15:55:12 +0200

    Renommage des variables de Sprite
    partie 1

--- src/RogueProject/Sprite.cs
+++ src/RogueProject/Sprite.cs
@@ -14,15 +14,16 @@ namespace RogueProject
         private Rectangle m_SpriteSheet_Size;
 
         private Texture2D m_Sprite_Tex2D;// Texture du Sprite
-        public  Vector2 m_Sprite_Pos;// Position du Sprite dans l'environement
+        private  Vector2 m_Sprite_Pos;// Position du Sprite dans l'environement
+        float m_Sprite_Velocity; // Vitesse de déplacement du Sprite 
         private Rectangle? m_SourceRectangle; // Taille du Sprite ??
         //private Vector2 m_Sprite_Size; // Taille du Sprite 
-        private Color m_Color; // Filtre appliqué sur le sprite
-        private float m_Rotation; // Angle de rotation a appliquer au sprite
-        private Vector2 m_Origin; //position d'orgine
-        private Vector2 m_Scale;
-        private SpriteEffects m_Effects;
-        private float m_LayerDepth;
+        //private Color m_Sprite_Color; // Filtre appliqué sur le sprite
+        private float m_Sprite_Rotation; // Angle de rotation a appliquer au sprite
+        private Vector2 m_Sprite_Origin; //position d'orgine
+        private Vector2 m_Sprite_Scale;
+        private SpriteEffects m_Sprite_Effect;
+        private float m_Sprite_LayerDepth;
 
         
    
@@ -38,33 +39,35 @@ namespace RogueProject
         // Effets de sprite (SpriteEffects.None pour aucun effet).
         //
 
-
-
-        float m_SpriteVelocity;
         bool m_IsSpriteSheet;
 
         public Sprite(
-            Texture2D _Sprite_Tex2D, 
+            Texture2D _Texture2D, 
             SpriteBatch _SpriteBatch, 
-            Vector2 _Sprite_Pos = new Vector2(),
-            Vector2 _SpriteVelocity = new Vector2(),
+            Vector2 _Position = new Vector2(),
+            Vector2 _Velocity = new Vector2(),
             Rectangle? m_SourceRectangle = null,
             /*Color _Color = Color.White,*/
             float _Rotation = 0f, 
             Vector2 _Origin = new Vector2(),
-            Vector2 m_Scale = new Vector2(),
-            SpriteEffects _Effects = SpriteEffects.None,
-            float m_LayerDepth = 0f) {
+            Vector2 _Scale = new Vector2(),
+            SpriteEffects _Effect = SpriteEffects.None,
+            float _LayerDepth = 0f) {
             
-            this.SetTexture(_Sprite_Tex2D);
-            this.SetPosition(_Sprite_Pos);
-            this.SetSourceRectangle();
-            //this.SetSize(_Sprite_Size);
+            this.SetTexture(_Texture2D);
+            this.SetPosition(_Position);
+            this.SetVelocity(_Velocity);
+            this.SetSourceRectangle(m_SourceRectangle);
+            this.SetRotation(_Rotation);
+            this.SetOrigin(_Origin);
+            this.SetScale(_Scale);
+            this.SetEffect(_Effect);
+            this.SetLayerDepth(_LayerDepth);
+            
             this.SetSpriteBatch(_SpriteBatch);
-            
 
 
-        }
+        }//this.SetSize(_Sprite_Size);
 
         public void SetTexture(Texture2D _Sprite_Tex2D) {
             m_Sprite_Tex2D = _Sprite_Tex2D;
@@ -74,6 +77,11 @@ namespace RogueProject
         public void SetPosition(Vector2 _Sprite_Pos)
         {
             m_Sprite_Pos = _Sprite_Pos;
+        }
+
+        public void SetVelocity(Vector2 _Velocity)
+        {
+            m_Sprite_Effect = spriteVelocity;
         }
 
         /*
@@ -94,12 +102,13 @@ namespace RogueProject
             m_SourceRectangle = sourceRectangle;
         }
 
+        /*
         // Setter pour m_Color
         public void SetColor(Color _Color)
         {
             m_Color = _Color;
         }
-
+        */
         // Setter pour m_Rotation
         public void SetRotation(float _Rotation)
         {
@@ -113,13 +122,13 @@ namespace RogueProject
         }
 
         // Setter pour m_Scale
-        public void SetSpriteScale(Vector2 _Scale)
+        public void SetScale(Vector2 _Scale)
         {
             m_Scale = _Scale;
         }
 
         // Setter pour m_Effects
-        public void SetEffects(SpriteEffects _Effects)
+        public void SetEffect(SpriteEffects _Effects)
         {
             m_Effects = _Effects;
         }
