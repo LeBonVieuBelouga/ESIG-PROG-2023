ba2be0ca8dffbdc259dab313efd6aeb958c06b24
Léo Küttel <leo.kuettel@gmail.com>
2023-09-11 16:06:07 +0200

    Création de la classe Entity
    +Class entity <- Entity.cs
    + Class entity <- UML

---
+++ src/RogueProject/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueProject
+{
+    internal class Entity : Sprite
+    {
+        public const int HEALTH_DEFAULT = 1;
+
+
+        private int m_HealthPoint;
+        private int m_Damage;
+        private int m_Defense;
+
+        public Entity(int _HealthPoint, int m_Damage, int _Defense) { 
+            
+        }
+
+        
+    }
+}
